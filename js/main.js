798bed40e2194ddc061ed906bb0abde4f6ba2f38
Mostafa Khaled <71064899+Mostafa-K-Fakhry@users.noreply.github.com>
2025-10-01 20:34:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,209 @@
+// start Category
+let Steak = ``;
+let Sushi = ``;
+let Appetizer = ``;
+let Pasta =``;
+Steak = ` <div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Steak Sauce.webp" alt="Steak Sauce" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Steak Sauce</h4>
+              <p class="text-white pt-3">
+                Tender beef steak, seared to perfection, served with a rich orange sauce made from fresh juice and zest, bringing a balance of tangy sweetness and savory flavor, then finished with a glossy glaze and a sprinkle of herbs on top.
+              </p>
+              <span>$40</span>
+            </div>
+          </div>
+          <div class="row row-size justify-content-center">
+            <div class="col-md-4 offset-md-1">
+              <h4 class="text-white text-center mt-5">Creamy Steak</h4>
+              <p class="text-white pt-3">
+                Juicy beef steak, tender and rich, paired with creamy mushroom sauce, smooth and flavorful, layered with golden seared edges, then finished with a drizzle of velvety cream, a touch of garlic, fresh herbs, and delicate pepper sprinkled on top.
+              </p>
+              <span>$35</span>
+            </div>
+            <div class="col-md-6 offset-md-1 ps-5">
+              <img src="imgs/Creamy Steak.webp" alt="Creamy Steak" />
+            </div>
+          </div>
+          <div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Smoked Steak.webp" alt="Smoked Steak" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Smoked Steak</h4>
+              <p class="text-white pt-3">
+                Tender smoked steak, slow-cooked for deep flavor, rich and juicy inside with a slightly crisp crust, infused with natural wood smoke, then finished with a drizzle of savory glaze, fresh herbs, roasted garlic, and delicate pepper sprinkled generously on top.
+              </p>
+              <span>$40</span>
+            </div>
+          </div>`;
+
+/////////////////////////////////////////////////////////////
+
+Sushi = `<div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Dragon Sushi.webp" alt="Dragon Sushi" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Dragon Sushi</h4>
+              <p class="text-white pt-3">
+                Fresh salmon slices, creamy avocado, crisp cucumber, and golden
+                tempura crunch, layered with flavorful rice, then finished with
+                spicy mayo, sweet teriyaki drizzle, and delicate sesame seeds
+                sprinkled on top.
+              </p>
+              <span>$20</span>
+            </div>
+          </div>
+          <div class="row row-size justify-content-center">
+            <div class="col-md-4 offset-md-1">
+              <h4 class="text-white text-center mt-5">Creamy Sushi</h4>
+              <p class="text-white pt-3">
+                Rich salmon, smooth avocado, tender crab, and soft rice, wrapped
+                with nori, topped with creamy cheese sauce, spicy mayo, and a
+                gentle drizzle of sweet teriyaki, then sprinkled with roasted
+                sesame seeds.
+              </p>
+              <span>$25</span>
+            </div>
+            <div class="col-md-6 offset-md-1 ps-5">
+              <img src="imgs/Creamy Sushi.webp" alt="Creamy Sushi" />
+            </div>
+          </div>
+          <div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Roll Salmon Sushi.webp" alt="Roll Salmon Sushi" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Roll Salmon Sushi</h4>
+              <p class="text-white pt-3">
+                Delicate salmon rolls, filled with creamy avocado, crisp
+                cucumber, and tender rice, wrapped in fresh nori sheets, topped
+                with spicy mayo, sweet teriyaki glaze, roasted sesame seeds, and
+                a touch of zesty ginger.
+              </p>
+              <span>$18</span>
+            </div>
+          </div>`;
+
+///////////////////////////////////////////////////////////////////////
+Pasta=`<div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Spaghetti Carbonara.webp" alt="Spaghetti Carbonara" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Spaghetti Carbonara</h4>
+              <p class="text-white pt-3">
+              Tender spaghetti tossed in a creamy Parmesan and egg sauce, with crispy pancetta, sautéed garlic, black pepper, and fresh herbs, finished with a gentle sprinkle of grated Parmesan on top.
+              </p>
+              <span>$22</span>
+            </div>
+          </div>
+          <div class="row row-size justify-content-center">
+            <div class="col-md-4 offset-md-1">
+              <h4 class="text-white text-center mt-5">Lasagna</h4>
+              <p class="text-white pt-3">
+              Layers of tender pasta, rich Bolognese, creamy béchamel, and melted mozzarella, seasoned with garlic and herbs, baked to golden perfection, finished with a sprinkle of Parmesan, delivering a hearty, comforting, and flavorful experience.
+              </p>
+              <span>$25</span>
+            </div>
+            <div class="col-md-6 offset-md-1 ps-5">
+              <img src="imgs/Lasagna.webp" alt="Lasagna" />
+            </div>
+          </div>
+          <div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Fettuccine Alfredo.webp" alt="Fettuccine Alfredo" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Fettuccine Alfredo</h4>
+              <p class="text-white pt-3">
+               Tender fettuccine noodles, coated in rich and creamy Alfredo sauce, layered with sautéed garlic, butter, and Parmesan, finished with a sprinkle of fresh parsley, cracked black pepper, and a hint of nutmeg, delivering a smooth, comforting, and indulgent flavor.
+              </p>
+              <span>$23</span>
+            </div>
+          </div>`;
+
+////////////////////////////////////////////////////////////////////////////////
+
+Appetizer = `<div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Gourmet Bites.webp" alt="Gourmet Bites" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Gourmet Bites</h4>
+              <p class="text-white pt-3">
+                Crispy gourmet bites, golden and light, filled with tender vegetables and creamy cheese, seasoned with delicate herbs, offering a rich yet balanced flavor, then finished with a drizzle of tangy sauce, fresh greens, roasted seeds, and a gentle sprinkle of pepper on top.
+              </p>
+              <span>$12</span>
+            </div>
+          </div>
+          <div class="row row-size justify-content-center">
+            <div class="col-md-4 offset-md-1">
+              <h4 class="text-white text-center mt-5">Tomato and Basil Bruschetta</h4>
+              <p class="text-white pt-3">
+                Crispy toasted bread, golden and warm, topped with juicy tomatoes and fresh basil leaves, drizzled with extra virgin olive oil for a smooth richness, then finished with a hint of garlic, sea salt, cracked pepper, and a gentle sprinkle of parmesan on top.
+              </p>
+              <span>$14</span>
+            </div>
+            <div class="col-md-6 offset-md-1 ps-5">
+              <img src="imgs/Tomato and Basil Bruschetta.webp" alt="Tomato and Basil Bruschetta" />
+            </div>
+          </div>
+          <div class="row row-size pt-5 justify-content-center">
+            <div class="col-md-6">
+              <img src="imgs/Fried Calamari.webp" alt="Fried Calamari" />
+            </div>
+            <div class="col-md-4">
+              <h4 class="text-white text-center mt-3">Fried Calamari</h4>
+              <p class="text-white pt-3">
+                Crispy fried calamari, golden and tender, lightly coated for a perfect crunch, seasoned with delicate herbs and sea salt, then served with a drizzle of zesty lemon, tangy marinara sauce, fresh parsley, roasted garlic, and a gentle sprinkle of pepper on top.
+              </p>
+              <span>$12</span>
+            </div>
+          </div>`;
+
+//////////////////////////////////////////
+const buttons = document.querySelectorAll(".btn-food");
+const category = document.querySelector(".category");
+
+category.innerHTML = Sushi;
+
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    buttons.forEach(function (b) {
+      b.classList.remove("active");
+    });
+
+    btn.classList.add("active");
+
+    if (btn.id === "Steak") category.innerHTML = Steak;
+    if (btn.id === "Sushi") category.innerHTML = Sushi;
+    if (btn.id === "Pasta") category.innerHTML = Pasta;
+    if (btn.id === "Appetizer") category.innerHTML = Appetizer;
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////
+// start Animation 
+const slides = document.querySelectorAll("#dining-slider .slide");
+let current = 0;
+
+setInterval(() => {
+  const currentSlide = slides[current];
+  currentSlide.classList.remove("active");
+  currentSlide.classList.add("exit-left");
+
+  current = (current + 1) % slides.length;
+  const nextSlide = slides[current];
+  nextSlide.classList.add("active");
+
+  setTimeout(() => {
+    currentSlide.classList.remove("exit-left");
+  }, 700);
+
+}, 3000); 
+//////////////////////////////////////////////////////////////////////////////
+
